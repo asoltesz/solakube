92d2bf55800fa3434b1f54890b46824d2eb72427
András Soltész <andras.soltesz@ixenit.com>
2019-11-21 17:15:29 +0100

    Fixing Helm version check + improved dependency checking script output

--- scripts/check-dependencies.sh
+++ scripts/check-dependencies.sh
@@ -1,26 +1,47 @@
 #!/usr/bin/env bash
+# ------------------------------------------------------------------------------
+#
+# Checks if all necessary software that are needed by the scripts (dependencies)
+# are present.
+#
+# WARNING: It does not (yet) check necessary minimum and maximum versions
+#
+# ------------------------------------------------------------------------------
+
+function hr() {
+    echo "----------------------------------------------"
+}
+
+ALL_PRESENT=0
 
 
-ALL_SATISFIED=0
+echo "Checking all dependencies and printing their version information"
+
 
 #-----------------------------------------
 # Helm
 #-----------------------------------------
+hr
+echo "Helm"
+hr
 
-HELM_VERSION="$(helm --version)"
+HELM_VERSION="$(helm version --client)"
 
 if [[ $? = 0 ]]
 then
     echo "Helm version: ${HELM_VERSION}"
 else
     echo "Helm not found"
-    ALL_SATISFIED=1
+    ALL_PRESENT=1
 fi
 
 
 #-----------------------------------------
 # Ansible
 #-----------------------------------------
+hr
+echo "Ansible"
+hr
 
 ANSIBLE_VERSION="$(ansible --version)"
 
@@ -29,12 +50,15 @@ then
     echo "Ansible version: ${ANSIBLE_VERSION}"
 else
     echo "Ansible not found"
-    ALL_SATISFIED=1
+    ALL_PRESENT=1
 fi
 
 #-----------------------------------------
 # Terraform
 #-----------------------------------------
+hr
+echo "Terraform"
+hr
 
 TERRAFORM_VERSION="$(terraform --version)"
 
@@ -43,10 +67,22 @@ then
     echo "Terraform version: ${TERRAFORM_VERSION}"
 else
     echo "Terraform not found."
-    ALL_SATISFIED=1
+    ALL_PRESENT=1
 fi
 
 
 
 #--------------------------------------
-exit ${ALL_SATISFIED}
+hr
+
+if [[ "${ALL_PRESENT}" == 0 ]]
+then
+    echo "RESULT: All dependencies are present"
+else
+    echo "RESULT: One or more dependencies are missing"
+fi
+
+hr
+
+
+exit ${ALL_PRESENT}
