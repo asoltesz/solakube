556f69cbb59c74dfdd4979241e9ee3addcfb3fd9
András Soltész <andras.soltesz@ixenit.com>
2019-12-27 20:28:28 +0100

    Configuration: configure-andromeda.sh script that is easier to source than ~/.secrets/terraform-andromeda.sh

---
+++ configure-andromeda.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+echo
+echo "Loading variables for the SolaKube installation process"
+echo
+
+. ~/.secrets/terraform-andromeda.sh
+
+echo "--------------------------------------------------------------------------"
+echo " SolaKube shell inited for the Andromeda Cluster "
+echo "--------------------------------------------------------------------------"
