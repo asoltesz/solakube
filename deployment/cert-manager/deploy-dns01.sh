1c772f44a3a89357f73b8711f3ddc0d9437c73ef
András Soltész <andras.soltesz@ixenit.com>
2019-12-27 20:33:06 +0100

    Cert-Manager now handles wildcard/dns01 certificates (CloudFlare-only) + all resource descriptors moved to separate files

---
+++ deployment/cert-manager/deploy-dns01.sh
@@ -0,0 +1,84 @@
+#!/usr/bin/env bash
+
+# ==============================================================================
+#
+# Installs the Lets Encrypt dns01 Issuer with CloudFlare
+#
+# This can be used to get a single wildcard TLS certificate for the whole
+# cluster and to be used by all services.
+#
+# Arguments:
+# 1 - operating mode. VALIDATE_ONLY or FULL.
+#     If VALIDATE, it stops after validating the parameters
+# ==============================================================================
+
+operatingMode=$1
+
+source ../shared.sh
+
+
+# Stop immediately if any of the deployments fail
+trap errorHandler ERR
+
+echoSection "Installing a CloudFlare / Let's Encrypt dns01 Issuer"
+
+defineNamespace "cert-manager"
+
+# ------------------------------------------------------------
+echoSection "Validating parameters"
+
+paramValidation "LETS_ENCRYPT_ACME_EMAIL" \
+   "the email address you want to present to Let's Encript as the person responsible for the certs of your domain."
+
+paramValidation "CLUSTER_FQN" \
+    "the FQN for the cluster, like 'andromeda.nostran.com' "
+
+paramValidation "CLOUDFLARE_EMAIL" \
+    "the Cloudflare administrator account's email address"
+
+paramValidation "CLOUDFLARE_API_KEY" \
+    "the Cloudflare administrator account API key"
+
+if [[ "VALIDATE_ONLY" == "${operatingMode}" ]]
+then
+    exit 0
+fi
+
+# ------------------------------------------------------------
+echoSection "Preparing temp folder"
+
+createTempDir "cert-manager-dns01"
+
+
+# ------------------------------------------------------------
+echo "Adding K8s objects for the Cluster FQN: ${CLUSTER_FQN}"
+
+export CLOUDFLARE_API_KEY_B64=$(echo -n "${CLOUDFLARE_API_KEY}" | base64 )
+
+applyTemplate cloudflare-api-key-secret.yaml
+
+applyTemplate cloudflare-issuer.yaml
+
+applyTemplate cluster-certificate.yaml
+
+
+# ------------------------------------------------------------
+echo "Waiting for the certificate to be issued"
+
+checkCertificateIssued cluster-fqn-certificate cert-manager
+
+echo "Making the new TLS secret replicatable accross namespaces"
+
+kubectl patch secret ${CLUSTER_CERT_SECRET_NAME} \
+  --type json -p \
+    '[{"op":"add","path":"/metadata/annotations/replicator.v1.mittwald.de~1replication-allowed", "value":"true"}]' \
+    --namespace cert-manager
+
+kubectl patch secret ${CLUSTER_CERT_SECRET_NAME} \
+  --type json -p \
+    '[{"op":"add","path":"/metadata/annotations/replicator.v1.mittwald.de~1replication-allowed-namespaces", "value": "'.*'"}]' \
+    --namespace cert-manager
+
+echoSection "Cert-manager's dns01 Issuer has been installed on your cluster'"
+# ------------------------------------------------------------
+
---
+++ deployment/replicator/deploy.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# ==============================================================================
+#
+# Installs the appropriate Ingress and cert-manager Certificate descriptor
+# for HTTPS access of pgAdmin
+#
+# WARNING: Assumes that a cert-manager ClusterIssuer named "letsencrypt-http01"
+# is already deployed on the cluster (it will define the Certificate to be
+# requested from)
+#
+# ==============================================================================
+
+# Internal parameters
+
+REPLICATOR_VERSION=2.0.1
+
+# ------------------------------------------------------------
+source ../shared.sh
+
+# Stop immediately if any of the deployments fail
+trap errorHandler ERR
+
+
+# ------------------------------------------------------------
+echoSection "Installing replicator v${REPLICATOR_VERSION}"
+
+kubectl apply -f \
+    https://raw.githubusercontent.com/mittwald/kubernetes-replicator/v${REPLICATOR_VERSION}/deploy/rbac.yaml
+
+kubectl apply -f \
+    https://raw.githubusercontent.com/mittwald/kubernetes-replicator/v${REPLICATOR_VERSION}/deploy/deployment.yaml
+
+# ------------------------------------------------------------
+echoSection "Replicator has been installed on your cluster"
+
