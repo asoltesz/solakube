c59d312d7964d8a1d682ddd9b55d6a02d77aa844
András Soltész <andras.soltesz@ixenit.com>
2020-04-07 22:05:57 +0200

    Documentation and improved typing for andromeda variables

--- terraform/clusters/andromeda/variables.tf
+++ terraform/clusters/andromeda/variables.tf
@@ -4,17 +4,116 @@ variable "ssh_private_key" {}
 variable "ssh_public_key" {}
 variable "rancher_api_url" {}
 variable "rancher_api_token" {}
-variable "etcd_backup_enabled" {}
-variable "etcd_s3_access_key" {}
-variable "etcd_s3_secret_key" {}
-variable "etcd_s3_bucket_name" {}
-variable "etcd_s3_endpoint" {}
-variable "etcd_s3_region" {}
-variable "servers" {}
+
+#
+# Whether the Rancher server is allowed to regularly backup the
+# etcd database of the cluster
+#
+variable "etcd_backup_enabled" {
+  type = bool
+}
+
+#
+# Access Key for the regular etcd backups (when stored in S3)
+#
+variable "etcd_s3_access_key" {
+  type = string
+}
+
+#
+# Secret Key for the regular etcd backups (when stored in S3)
+#
+variable "etcd_s3_secret_key" {
+  type = string
+}
+
+#
+# Name of the S3 Bucket for the regular etcd backups (when stored in S3)
+#
+variable "etcd_s3_bucket_name" {
+  type = string
+}
+
+#
+# The URL of the S3 endpoint for the regular etcd backups (when stored in S3)
+#
+variable "etcd_s3_endpoint" {
+  type = string
+}
+
+#
+# The S3 region name for the regular etcd backups (when stored in S3)
+#
+variable "etcd_s3_region" {
+  type = string
+}
+
+#
+# Whether Prometheus monitoring is allowed on the cluster
+#
+variable enable_cluster_monitoring {
+  type = bool
+  default = false
+}
+
+#
+# Whether Prometheus alerting is allowed on the cluster
+#
+variable enable_cluster_alerting {
+  type = bool
+  default = false
+}
+
+
+#
+# Server nodes that will be part of the Kubernetes cluster
+#
+variable "servers" {
+  type = map(
+    object({
+      # Name of the node
+      name               = string,
+      # The private address of the node
+      private_ip_address = string,
+      # Hetzner server type
+      server_type        = string,
+      # Hetzner base OS image
+      image              = string,
+      # Datacenter location
+      location           = string,
+      # Whether backups are needed for it
+      backups            = bool,
+      # Cloud-Init script reference (see user_data_scripts map)
+      user_data_script   = string,
+      # Rancher/K8s node roles (e.g.: "--worker --etcd --controlplane)
+      roles              = string
+    })
+  )
+}
+
+
+#
+# The Kubernetes version you want to install on the nodes.
+#
+# It must be one of the supported versions of your Rancher/RKE server
+#
+variable "kubernetes_version" {
+  default = "v1.15.11-rancher1-2"
+}
+
+
 variable "ansible_playbook_path" {}
 variable "ansible_vault_password_path" {}
 variable "ingress_provider" {}
 
+#
+# Whether the Rancher/RKE deployment should be run on the nodes
+# Without this, the nodes will not start to deploy RKE and will not join
+# the cluster
+#
+variable run_rancher_deploy {
+  default = true
+}
 
 
 // -----------------------------------------------------------------------------
@@ -30,8 +129,8 @@ variable "ingress_provider" {}
 // partition would consume all space on the disk and we want custom partitioning
 //
 // Then we create a custom partition layout, like this:
-// # 0    0    - 10 GB - ext4 on /    (OS files)
-// # 1   10 GB - 100%  - lvm (for Ceph)
+// # 0    0    - 15 GB - ext4 on /    (OS files)
+// # 1   15 GB - 100%  - lvm (for Ceph)
 //
 // -----------------------------------------------------------------------------
 
@@ -44,9 +143,11 @@ variable "user_data_scripts" {
     //
     // Generic Cloud Init script for CentOS 7 VMs
     //
-    // 10 GB is reserved for the OS and Docker images, all the rest goes to
+    // 15 GB is reserved for the OS and Docker images, all the rest goes to
     // the Rook/Ceph storage cluster in the form of the /dev/sda2 partition.
     //
+    // sda2 is left without a filesystem because Ceph doesn't accept
+    // if there was an fs on it
     centos7_generic = <<EOF
 #cloud-config
 
@@ -54,13 +155,20 @@ growpart:
   mode: off
 
 runcmd:
-  - [ parted, "/dev/sda", mkpart, primary, ext2, "10GB", "100%" ]  # create a new partition, starting at 10GB
-  - [ parted, "/dev/sda", set, "2", lvm, on ]  # set LVM flag
-  - [ growpart, "/dev/sda", "1" ]  # grow first partition to 10GB
-  - [ partx, --update, "/dev/sda" ] # reload partition table
-  - [ resize2fs, /dev/sda1 ] # resize first partition (/) to 10GB
-  - [ pvcreate, "/dev/sda2" ] # create PV on /dev/sda2 (100%-10GB)
-  - [ vgcreate, vg1, "/dev/sda2" ] # create VG, adding PV /dev/sda2
+  # create a new partition, starting at 15GB
+  - [ parted, "/dev/sda", mkpart, primary, ext2, "15GB", "100%" ]
+  # set LVM flag on the new partition
+  - [ parted, "/dev/sda", set, "2", lvm, on ]
+  # grow first partition to fill all the allocated space
+  - [ growpart, "/dev/sda", "1" ]
+  # reload partition table
+  - [ partx, --update, "/dev/sda" ]
+  # resize filesystem to fill the first partition completely
+  - [ resize2fs, /dev/sda1 ]
+#  # create PV on /dev/sda2
+#  - [ pvcreate, "/dev/sda2" ]
+#  # create VG, adding PV /dev/sda2
+#  - [ vgcreate, vg1, "/dev/sda2" ]
 
 repo_update: true
 repo_upgrade: all
@@ -69,5 +177,6 @@ packages:
   - lvm2  # missing in the Hetzner image
 
 EOF
+
   }
 }
