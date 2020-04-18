185e0f563f937f93db6b30d67101bcc3a700e7f3
András Soltész <andras.soltesz@ixenit.com>
2020-04-18 15:59:26 +0200

    Checking Terraform's success in the sk-apply script (better error handling)

--- scripts/sk-apply.sh
+++ scripts/sk-apply.sh
@@ -8,21 +8,20 @@
 
 
 ${SK_SCRIPTS_HOME}/sk-tf.sh apply "$@"
+checkResultExit "Terraform apply"
 
-if [[ $? == 0 ]]
+# The ID of the newly created cluster in your Rancher installation
+RANCHER_CLUSTER_ID="$(${SK_SCRIPTS_HOME}/sk-tf.sh output "rancher_cluster_id")"
+
+if [[ $? != 0 ]]
 then
-    # The ID of the newly created cluster in your Rancher installation
-    RANCHER_CLUSTER_ID="$(${SK_SCRIPTS_HOME}/sk-tf.sh output "rancher_cluster_id")"
-
-    if [[ $? != 0 ]]
-    then
-        echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: ${RANCHER_CLUSTER_ID}"
-        echo "WARNING: Failed to load the cluster id from the Terraform state"
-        exit 1
-    fi
-
-    # Exporting the cluster ID for the other scripts to load
-    file=~/.solakube/${SK_CLUSTER}/rancher_cluster_id.sh
-    echo "export RANCHER_CLUSTER_ID=${RANCHER_CLUSTER_ID}" > ${file}
-    chmod +x ${file}
+    echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!: ${RANCHER_CLUSTER_ID}"
+    echo "WARNING: Failed to load the cluster id from the Terraform state"
+    exit 1
 fi
+
+# Exporting the cluster ID for the other scripts to load
+file=~/.solakube/${SK_CLUSTER}/rancher_cluster_id.sh
+echo "export RANCHER_CLUSTER_ID=${RANCHER_CLUSTER_ID}" > ${file}
+chmod +x ${file}
+
