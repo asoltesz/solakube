7ed6ffdb741e13660c3cac9ec32bcde925f6eaff
András Soltész <andras.soltesz@ixenit.com>
2020-07-26 19:48:32 +0200

    Nextcloud: Force session disconnects when dropping the database

--- deployment/nextcloud/drop-pg-database.sql
+++ deployment/nextcloud/drop-pg-database.sql
@@ -11,6 +11,14 @@
 \echo '------------------'
 \echo ''
 
+-- Stop new connections. Superusers still can connect!
+ALTER DATABASE ${NEXTCLOUD_APP_NAME} CONNECTION LIMIT 0;
+
+-- Force disconnection of all clients connected to this database, using pg_terminate_backend.
+SELECT pg_terminate_backend(pid)
+  FROM pg_stat_activity
+ WHERE datname = '${NEXTCLOUD_APP_NAME}';
+
 DROP DATABASE ${NEXTCLOUD_APP_NAME};
 DROP USER ${NEXTCLOUD_APP_NAME};
 
